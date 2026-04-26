a9b387c3dcbb531ffb498c5bd117d5a92d9917f0
Rhoan Jean <rhoandeguzman75@gmail.com>
2026-04-26 17:12:24 +0800

    tang ina

--- config/database.php
+++ config/database.php
@@ -1,1 +1,7 @@
-kgjgggggggggggggggggggggg
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'flowershop_db');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_CHARSET', 'utf8mb4');
